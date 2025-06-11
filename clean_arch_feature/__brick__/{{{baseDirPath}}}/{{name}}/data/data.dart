891090c4b758b65594b5e2361e52bf395319d562
Dmytro Meleshko <meleshkodim2000@gmail.com>
2025-06-11 17:39:26 +0200

    feat(clean_arch_feature): Add base template

---
+++ clean_arch_feature/__brick__/{{{baseDirPath}}}/{{name}}/data/data.dart
@@ -0,0 +1,4 @@
+export 'data_sources/data_sources.dart';
+export 'mappers/mappers.dart';
+export 'models/models.dart';
+export 'repositories/repositories.dart';
---
+++ clean_arch_feature/__brick__/{{{baseDirPath}}}/{{name}}/domain/domain.dart
@@ -0,0 +1,4 @@
+export 'entities/entities.dart';
+export 'interactors/interactors.dart';
+export 'repositories/repositories.dart';
+export 'values/values.dart';
